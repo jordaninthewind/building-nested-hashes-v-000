8df75b0d542fb5f3f4513eee081845f497ffa66e
Jordan Kline <jordan.kline@gmail.com>
2018-01-06 17:47:21 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,8 +14,8 @@ def third_challenge
         :age => {"15"},
         :status => {"alive"}
         :hero_friends => []
-        },
-   },
+        }
+      },
    :capulet => {
       :patriarch => {
         :name => {"Lord Capulet"},
